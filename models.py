99f0ccaf72f069c2bc01ce1602383b15b5e29241
Klaus-Uwe Mitterer <git@kumi.email>
2020-01-17 16:24:53 +0100

    Comply with MySQL CharField restriction of 255 chars

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Setting(models.Model):
-    key = models.CharField(primary_key=True, max_length=512)
-    value = models.CharField(max_length=512)
+    key = models.CharField(primary_key=True, max_length=255)
+    value = models.CharField(max_length=255)
 
     def __str__(self):
         return self.key
