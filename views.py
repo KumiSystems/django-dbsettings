264a433574bbaec35f96c212908e76cf8f4423bb
Klaus-Uwe Mitterer <git@kumi.email>
2020-04-08 16:43:56 +0200

    Add default return value argument to getValue()

--- views.py
+++ views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from dbsettings.models import Setting
 
-def getValue(key):
+def getValue(key, default=None):
     try:
         return Setting.objects.get(key=key).value
     except:
-        raise KeyError("No such setting: %s" % key)
+        if default is None:
+            raise KeyError("No such setting: %s" % key)
+        else:
+            return default
 
 def setValue(key, value):
     obj = Setting.objects.get_or_create(key=key)[0] # pylint: disable=E1101
